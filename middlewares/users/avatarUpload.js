09b480c99820a40f15c4f5a20f1792c963137188
pronazmul <developernazmul@gmail.com>
2021-06-08 21:51:23 +0600

    Multer Upload Object Setup

---
+++ middlewares/users/avatarUpload.js
@@ -0,0 +1,30 @@
+// Internal Modules
+const singleUploader = require("../../utilities/singleUploader");
+
+// Pass Single Uploder to Create Multar Upload Object
+const avatarUpload = (req, res, next) => {
+  const upload = singleUploader(
+    "avatars",
+    ["image/jpeg", "image/jpg", "image/png"],
+    1000000,
+    "Only .jpg, jpeg or .png format allowed!"
+  );
+
+  //   Call The Multer Upload MiddleWare Function To Handle Error:
+  upload.any()(req, res, (error) => {
+    if (error) {
+      res.status(500).json({
+        errors: {
+          avatar: {
+            msg: error.message,
+          },
+        },
+      });
+    } else {
+      next();
+    }
+  });
+};
+
+// Module Exports
+module.exports = avatarUpload;
